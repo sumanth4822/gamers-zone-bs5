c3a85683f3035efc578051ecefc281f71a3fa6c9
sumanth4822 <sumanth009sada@gmail.com>
2023-07-11 22:30:48 +0530

    object

--- JAVASCRIPT/objects/obj.js
+++ JAVASCRIPT/objects/obj.js
@@ -1,41 +1,62 @@
-const person = {
-    name: "Rocky",
-    age: 30,
-    greet: function () {
-        console.log("Hey Rocky");
-    }
-};
-const gamersZone = {
-    name: "Gamers Zone",
-    location: "City X",
-    games: ["Game A", "Game B", "Game C"],
-    isOpen: true,
-    showLocation: function () {
-        console.log("Location: " + this.location);
-    },
-    showGames: function () {
-        console.log("Games available:");
-        for (let i = 0; i < this.games.length; i++) {
-            console.log("- " + this.games[i]);
-        }
-    },
-    toggleOpen: function () {
-        // this.isOpen = !this.isOpen;
-        console.log("Zone is now " + (this.isOpen ? "open" : "closed") + ".");
-    }
-};
+// const person = {
+//     name: "DIME",
+//     age: 23,
+//     greet: function () {
+//         console.log("WELCOME TO GAMING");
+//     }
+// };
+// const gamersZone = {
+//     name: "Gamers Zone",
+//     location: "INDIA",
+//     games: ["FORTNITE", "COD", "VALORANT"],
+//     isOpen: true,
+//     showLocation: function () {
+//         console.log("Location: " + this.location);
+//     },
+//     showGames: function () {
+//         console.log("Games available:");
+//         for (let i = 0; i < this.games.length; i++) {
+//             console.log("- " + this.games[i]);
+//         }
+//     },
+//     toggleOpen: function () {
+//         // this.isOpen = !this.isOpen;
+//         console.log("Zone is now " + (this.isOpen ? "open" : "closed") + ".");
+//     }
+// };
 
-console.log(gamersZone.name);        
-gamersZone.showLocation();           
+// console.log(person.name)
+// console.log(gamersZone.name);        
+// gamersZone.showLocation();           
 
-gamersZone.toggleOpen();            
+// gamersZone.toggleOpen();            
 
-const keys = Object.keys(gamersZone);
-console.log(keys); 
+// const keys = Object.keys(gamersZone);
+// console.log(keys); 
 
-const values = Object.values(gamersZone);
-console.log(values);
+// const values = Object.values(gamersZone);
+// console.log(values);
 
 
-const entries = Object.entries(gamersZone);
-console.log(entries);
+// const entries = Object.entries(gamersZone);
+// console.log(entries);
+
+// Object.freeze(gamersZone);
+// gamersZone.name = "New Name";
+// console.log(gamersZone.name); 
+
+
+
+//constructor
+function person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.greet = function () {
+        console.log("WELCOME");
+    };
+}
+
+const person2 = new person("Rakesh", 22);
+
+person2.name;
+person2.greet()
