72ff6e412c1f519862baaa5d1545889678f30f12
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-07-19 17:48:41 +0530

    changes

---
+++ DSA/vikash/3sum.js
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/3sum/
+
+
+const threeSum = function (nums) {
+    const result = [];
+    nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i !== 0 && nums[i] === nums[i - 1])
+            continue;
+
+        let left = i + 1, right = nums.length - 1;
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+
+            if (sum === 0) {
+                result.push([nums[i], nums[left], nums[right]]);
+
+                while (left < right && nums[left] == nums[left + 1])
+                    left++;
+
+                left++;
+                right--;
+            }
+
+            else if (sum < 0)
+                left++;
+
+            else
+                right--;
+        }
+    }
+
+    return result;
+}
